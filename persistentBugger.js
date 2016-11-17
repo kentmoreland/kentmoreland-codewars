8629c1e1b3642edc9d59f2919c33f55d3c911f9e
Kent <kentmoreland@gmail.com>
2016-11-17 08:52:49 -0600

    completed persistent bugger

---
+++ persistentBugger.js
@@ -0,0 +1,32 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+const persistence = (num) => {
+  let counter = 0; // tally
+  //recursive fn to get solution
+  const check = (num) => {
+    //check for single digit number
+    if (num / 10 < 1){
+      return;
+    }else{
+      //get new num to check
+      num = num.toString().split('').reduce((total, num) => {
+        return total *= num;
+      });
+      //tally count | keep going
+      counter++;
+      check(num);
+    }
+  };
+  //initiation and ending
+  check(num);
+  return counter;
+};
+
+
+console.log(persistence(39));// === 3  because 3*9 = 27, 2*7 = 14, 1*4=4
+                      // and 4 has only one digit
+
+console.log(persistence(999)); // === 4 because 9*9*9 = 729, 7*2*9 = 126,
+                       // 1*2*6 = 12, and finally 1*2 = 2
+
+console.log(persistence(4)); // === 0  because 4 is already a one-digit number
